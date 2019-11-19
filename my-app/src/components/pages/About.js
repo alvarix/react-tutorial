ac6c187cf9142c0fb6891eb0d636fdc1cfa28ca6
Alvar Sirlin <alvar@alvarsirlin.com>
2019-11-19 18:31:07 -0500

    dunno

---
+++ my-app/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About</h1>
+      <p>App to search Githubs.</p>
+      <p>V 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
